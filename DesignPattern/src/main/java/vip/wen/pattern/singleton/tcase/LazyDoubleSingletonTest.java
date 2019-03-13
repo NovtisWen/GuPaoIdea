994ba84884e936b448295cdb74adc5d574fdc0ee
NovtisWen <421971596@qq.com>
2019-03-13 18:46:28 +0800

    update singleton

--- DesignPattern/src/main/java/vip/wen/pattern/singleton/tcase/LazyDoubleSingletonTest.java
+++ DesignPattern/src/main/java/vip/wen/pattern/singleton/tcase/LazyDoubleSingletonTest.java
@@ -1,9 +1,12 @@
 package vip.wen.pattern.singleton.tcase;
 
 import vip.wen.pattern.singleton.demo.LazyDoubleSingleton;
+import vip.wen.pattern.singleton.demo.LazySimpleSingleton;
+
+import java.lang.reflect.Constructor;
 
 public class LazyDoubleSingletonTest {
-
+    /*简单多线程测试单例
     public static void main(String[] args){
         LazyDoubleSingletonTest lazy = new LazyDoubleSingletonTest();
         DoubleThread d1 = lazy.new DoubleThread();
@@ -24,5 +27,26 @@ public class LazyDoubleSingletonTest {
 
             System.out.println(Thread.currentThread().getName()+"： "+singleton);
         }
+    }*/
+
+    /**
+     * 反射破坏Simple懒加载单例
+     * @param args
+     */
+    public static void main(String[] args){
+        try{
+            LazyDoubleSingleton simple1 = LazyDoubleSingleton.getInstance();
+            Class clazz = LazyDoubleSingleton.class;
+
+            Constructor constructor = clazz.getDeclaredConstructor(null);
+            constructor.setAccessible(true);
+            LazyDoubleSingleton simple2 = (LazyDoubleSingleton) constructor.newInstance();
+            System.out.println("simple1:"+simple1);
+            System.out.println("simple2:"+simple2);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
--- DesignPattern/src/main/java/vip/wen/pattern/singleton/tcase/LazySimpleSingletonTest.java
+++ DesignPattern/src/main/java/vip/wen/pattern/singleton/tcase/LazySimpleSingletonTest.java
@@ -1,7 +1,14 @@
 package vip.wen.pattern.singleton.tcase;
 
+import jdk.internal.org.objectweb.asm.tree.TryCatchBlockNode;
+import vip.wen.pattern.singleton.demo.LazySimpleSingleton;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 public class LazySimpleSingletonTest {
 
+    /*简单多线程测试
     public static void main(String[] args){
         try {
             ExecutorThread t1 = new ExecutorThread();
@@ -14,6 +21,28 @@ public class LazySimpleSingletonTest {
             e.printStackTrace();
 
         }
+    }*/
+
+    /**
+     * 反射破坏Simple懒加载单例
+     * @param args
+     */
+    public static void main(String[] args){
+        try{
+            LazySimpleSingleton simple1 = LazySimpleSingleton.getInstance();
+            Class clazz = LazySimpleSingleton.class;
+
+            Constructor constructor = clazz.getDeclaredConstructor(null);
+            constructor.setAccessible(true);
+            LazySimpleSingleton simple2 = (LazySimpleSingleton) constructor.newInstance();
+            System.out.println("simple1:"+simple1);
+            System.out.println("simple2:"+simple2);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
+
 
 }
