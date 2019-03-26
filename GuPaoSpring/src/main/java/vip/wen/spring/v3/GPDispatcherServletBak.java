193bd70a752ad754b7ee2287f5f7899af3c306cc
NovtisWen <421971596@qq.com>
2019-03-27 02:21:42 +0800

    update spring

--- GuPaoSpring/src/main/java/vip/wen/spring/v3/GPDispatcherServletBak.java
+++ GuPaoSpring/src/main/java/vip/wen/spring/v3/GPDispatcherServletBak.java
@@ -1,7 +1,5 @@
-package vip.wen.spring.v2;
+package vip.wen.spring.v3;
 
-import com.sun.xml.internal.bind.unmarshaller.DOMScanner;
-import jdk.nashorn.internal.ir.IfNode;
 import vip.wen.spring.Service.GPService;
 import vip.wen.spring.annotation.GPAutowired;
 import vip.wen.spring.annotation.GPRequestMapping;
@@ -22,7 +20,7 @@ import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.*;
 
-public class GPDispatcherServlet extends HttpServlet {
+public class GPDispatcherServletBak extends HttpServlet {
     //保存application.properties配置文件中的内容
     private Properties contextConfig = new Properties();
     //保存扫描的所有的类名
